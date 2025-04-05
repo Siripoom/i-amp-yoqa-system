bbf8186d02f869a0f5b995a9f7677bdb81f3ef82
Siripoom <artorsiriratpoom@gmail.com>
2025-04-05 16:18:32 +0000

    Apr 5, 2025, 11:18 PM

--- front-end/src/services/classService.js
+++ front-end/src/services/classService.js
@@ -18,6 +18,7 @@ const classService = {
   getAllClasses: async () => {
     try {
       const response = await axios.get(`${API_URL}/api/classes`);
+      console.log(response.data);
       return response.data;
     } catch (error) {
       throw error.response ? error.response.data : error;
