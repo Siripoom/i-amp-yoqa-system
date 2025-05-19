7e7442a0b019199f2d80b8de6b3aeab412417d8c
Siripoom <artorsiriratpoom@gmail.com>
2025-05-19 18:30:34 +0000

    May 20, 2025, 1:30 AM

--- back-end/models/master.js
+++ back-end/models/master.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const masterSchema = new mongoose.Schema(
   {
     mastername: { type: String, required: true },
-    image: { type: String, required: false }, // เก็บชื่อไฟล์ที่ใช้ใน GridFS
-    video: { type: String, required: false },
+    image: { type: String, required: false }, // เก็บ URL ของรูปภาพ
+    videoUrl: { type: String, required: false }, // เก็บลิงก์วิดีโอจาก YouTube
+  
   },
   { timestamps: true }
 );
