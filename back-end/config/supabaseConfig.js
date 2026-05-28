839e57a7700b1f121b86cfcf74a39f9d30f0ef3d
Siripoom <artorsiriratpoom@gmail.com>
2026-05-28 11:55:34 +0700

    update

--- back-end/config/supabaseConfig.js
+++ back-end/config/supabaseConfig.js
@@ -1,9 +1,12 @@
 const { createClient } = require("@supabase/supabase-js");
 const dotenv = require("dotenv");
+const ws = require("ws");
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: { transport: ws },
+});
 
 module.exports = supabase;
